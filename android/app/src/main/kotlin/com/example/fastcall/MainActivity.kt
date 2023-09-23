2297d2ac1a297413aee0ff7c95070bb99a42763a
KritSrilerk <Kritsrilerk412@gmail.com>
2023-09-23 17:39:36 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/fastcall/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fastcall
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:fastcall/views/home_ui.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeUI(),
+    ),
+  );
+}
